5cb7366082c74f490e5ddbb45c7ee9c0595b1a91
mliedo05 <mliendo05@gmail.com>
2021-10-04 21:15:47 -0300

    create model order

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :orders
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+  }
   resources :products
   resources :brands
   resources :subcategories
