2c90a71f21c2937e14b8e77be5b98aa7cd42d014
mliedo05 <mliendo05@gmail.com>
2021-10-09 21:49:55 -0300

    change form edit user

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :orders
   devise_for :users, controllers: {
     sessions: 'users/sessions',
+    registrations: 'users/registrations',
   }
   resources :products
   resources :brands
